11eeb6a69ba5d50b9cf0111b31736fdafc9a2a85
Dennis Kimble <kimbled@tsbvi.edu>
2017-08-31 16:09:27 -0500

    Added weblinks migration

---
+++ src/Plugin/migrate/source/JoomlaWeblinks.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Drupal\joomla_migrate\Plugin\migrate\source;
+
+use Drupal\migrate\Plugin\migrate\source\SqlBase;
+use Drupal\migrate\Row;
+
+/**
+ * Source plugin for beer content.
+ *
+ * @MigrateSource(
+ *   id = "joomla_weblinks"
+ * )
+ */
+class JoomlaWeblinks extends SqlBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    /**
+     * An important point to note is that your query *must* return a single row
+     * for each item to be imported. Here we might be tempted to add a join to
+     * migrate_example_beer_topic_node in our query, to pull in the
+     * relationships to our categories. Doing this would cause the query to
+     * return multiple rows for a given node, once per related value, thus
+     * processing the same node multiple times, each time with only one of the
+     * multiple values that should be imported. To avoid that, we simply query
+     * the base node data here, and pull in the relationships in prepareRow()
+     * below.
+     */
+    $config = \Drupal::config('joomlamigrate.settings');
+    $database = $config->get('database');
+    $prefix = $config->get('table_prefix');
+    $query = $this->select($prefix . 'weblinks', 'jweb');
+    //$query->join($prefix . 'contentitem_tag_map', 'jctm', 'jcon.id = jctm.content_item_id');
+    $query->fields('jweb', [
+                    'id',
+                    'catid',
+                    'title',
+                    'alias',
+                    'url',
+                    'description',
+                    'created_by',
+                    'language']);
+    // $query->fields('jctm', [
+     //               'tag_id']);
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    $fields = [
+      'id' => $this->t('ID'),
+      'catid' => $this->t('Category ID'),
+      'title' => $this->t('Title'),
+      'alias' => $this->t('Article URL'),
+      'url' => $this->t('URLs'),
+      'description' => $this->t('Description'),
+      'created_by' => $this->t('Author ID'),
+      'language' => $this->t('Language'),
+    ];
+
+    return $fields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    return [
+      'id' => [
+        'type' => 'integer',
+        // This is an optional key currently only used by SqlBase.
+        'alias' => 'jweb',
+      ],
+    ];
+  }
+
+}
